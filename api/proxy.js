520bb1a1fcfc90d51bc9c2e9a5b48a76be2e477b
SA-UnknownSMP <unknownsmp49@gmail.com>
2025-05-29 10:52:27 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://c8kylx-3000.csb.app/mc-ip');
+  const response = await fetch('https://c8kykx-3000.csb.app/mc-ip');
   const data = await response.json();
   res.status(200).json(data);
 }
